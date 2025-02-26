8eae72fa953d4ff7c0e130ea26b0ac426f1d1efe
ibondarev <40579629+ibondarev-gsu@users.noreply.github.com>
2025-02-26 22:27:49 +0300

    commit

--- x/rollup/keeper/msg_server_submit_rollup_tx.go
+++ x/rollup/keeper/msg_server_submit_rollup_tx.go
@@ -2,6 +2,8 @@ package keeper
 
 import (
 	"context"
+	"encoding/binary"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/ibondarev-gsu/base/x/rollup/types"
@@ -10,8 +12,39 @@ import (
 func (k msgServer) SubmitRollupTx(goCtx context.Context, msg *types.MsgSubmitRollupTx) (*types.MsgSubmitRollupTxResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// Логируем полученные данные
+	k.Logger().Info("Received Rollup Tx", "data", msg.Data)
+
+	// Открываем KVStore через KVStoreService
+	store := k.storeService.OpenKVStore(ctx)
+
+	// Получаем текущий счетчик транзакций
+	txCountBz, err := store.Get([]byte("tx_count"))
+	if err != nil {
+		return nil, err
+	}
+
+	var txCount int64
+	if txCountBz == nil {
+		txCount = 0
+	} else {
+		txCount = int64(binary.BigEndian.Uint64(txCountBz))
+	}
+
+	// Увеличиваем счетчик и сохраняем новую транзакцию
+	txCount++
+	err = store.Set([]byte(fmt.Sprintf("rollup_tx_%d", txCount)), []byte(msg.Data))
+	if err != nil {
+		return nil, err
+	}
+
+	// Сохраняем обновленный счетчик транзакций
+	txCountBz = make([]byte, 8)
+	binary.BigEndian.PutUint64(txCountBz, uint64(txCount))
+	err = store.Set([]byte("tx_count"), txCountBz)
+	if err != nil {
+		return nil, err
+	}
 
 	return &types.MsgSubmitRollupTxResponse{}, nil
 }
